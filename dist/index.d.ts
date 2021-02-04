84d5668250131fe182e8bcf8e6b03e2cbaa3b787
Yasser Lahbibi <yasser.lahbibi@apenhet.com>
2021-02-04 15:06:28 +0100

    feat(state): fix  state types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,11 +1,30 @@
 import { ComputedRef, Ref } from 'vue-demi';
 declare type StorePath = typeof import('@store');
-declare type Store = StorePath['default'];
+declare type BaseStore = StorePath['default'];
 declare type Modules = StorePath['modules'];
 declare type ModuleNames = Extract<keyof Modules, string>;
-export declare function registerStore(store: Store): any;
-export declare function useModuleStore<ModuleName extends ModuleNames>(name: ModuleName): {
-    [x: string]: Ref<any> | ComputedRef<any> | ((payload: unknown) => any);
+declare type ModuleActions<ModuleName extends ModuleNames> = Extract<Modules[ModuleName], {
+    actions: any;
+}>['actions'];
+declare type ModuleActionNames<ModuleName extends ModuleNames> = Extract<keyof ModuleActions<ModuleName>, string>;
+declare type ModuleAction<ModuleName extends ModuleNames, ActionName extends ModuleActionNames<ModuleName>> = Extract<ModuleActions<ModuleName>[ActionName], (...args: any) => any>;
+declare type ModuleActionPayload<ModuleName extends ModuleNames, ActionName extends ModuleActionNames<ModuleName>> = Parameters<ModuleAction<ModuleName, ActionName>>[1] extends undefined ? () => ReturnType<ModuleAction<ModuleName, ActionName>> : (payload: Parameters<ModuleAction<ModuleName, ActionName>>[1]) => ReturnType<ModuleAction<ModuleName, ActionName>>;
+declare type ModuleActionsTree<ModuleName extends ModuleNames> = {
+    [K in ModuleActionNames<ModuleName>]: ModuleActionPayload<ModuleName, K>;
 };
+declare type ModuleGetters<ModuleName extends ModuleNames> = Extract<Modules[ModuleName], {
+    getters: any;
+}>['getters'];
+declare type ModuleGetterNames<ModuleName extends ModuleNames> = Extract<keyof ModuleGetters<ModuleName>, string>;
+declare type ModuleGetter<ModuleName extends ModuleNames, GetterName extends ModuleGetterNames<ModuleName>> = Extract<ModuleGetters<ModuleName>[GetterName], (...args: any) => any>;
+declare type ModuleGettersTree<ModuleName extends ModuleNames> = {
+    [K in ModuleGetterNames<ModuleName>]: ComputedRef<ReturnType<ModuleGetter<ModuleName, K>>>;
+};
+declare type ModuleState<ModuleName extends ModuleNames> = Modules[ModuleName]['state'] extends () => any ? ReturnType<Modules[ModuleName]['state']> : Modules[ModuleName]['state'];
+declare type ModuleStateTree<ModuleName extends ModuleNames> = {
+    [K in keyof ModuleState<ModuleName>]: Ref<ModuleState<ModuleName>[K]>;
+};
+export declare function registerStore<CurrentStore extends BaseStore>(store: CurrentStore): CurrentStore;
+export declare function useModuleStore<ModuleName extends ModuleNames>(name: ModuleName): ModuleActionsTree<ModuleName> & ModuleGettersTree<ModuleName> & ModuleStateTree<ModuleName>;
 export {};
 //# sourceMappingURL=index.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,12 @@ import { ComputedRef, Ref, computed, toRefs } from 'vue-demi'
 
 import { Module } from 'vuex'
 
-// @ts-ignore
 type StorePath = typeof import('@store')
-
-type Store = StorePath['default']
+type BaseStore = StorePath['default']
 type Modules = StorePath['modules']
-type RootState = Store['state'] & { [K in ModuleNames]: Modules[K] }
+type RootState = BaseStore['state'] & { [K in ModuleNames]: Modules[K] }
 type ModuleNames = Extract<keyof Modules, string>
 
-type ModuleState<ModuleName extends ModuleNames> = Modules[ModuleName]['state'] extends () => any ? ReturnType<Modules[ModuleName]['state']> : Modules[ModuleName]['state']
 type ModuleActions<ModuleName extends ModuleNames> = Extract<Modules[ModuleName], { actions: any }>['actions']
 type ModuleActionNames<ModuleName extends ModuleNames> = Extract<keyof ModuleActions<ModuleName>, string>
 type ModuleAction<ModuleName extends ModuleNames, ActionName extends ModuleActionNames<ModuleName>> = Extract<ModuleActions<ModuleName>[ActionName], (...args: any) => any>
@@ -18,23 +15,29 @@ type ModuleActionPayload<ModuleName extends ModuleNames, ActionName extends Modu
     () => ReturnType<ModuleAction<ModuleName, ActionName>> :
     (payload: Parameters<ModuleAction<ModuleName, ActionName>>[1]) => ReturnType<ModuleAction<ModuleName, ActionName>>
 type ModuleActionsTree<ModuleName extends ModuleNames> = { [K in ModuleActionNames<ModuleName>]: ModuleActionPayload<ModuleName, K> }
+
 type ModuleGetters<ModuleName extends ModuleNames> = Extract<Modules[ModuleName], { getters: any }>['getters']
 type ModuleGetterNames<ModuleName extends ModuleNames> = Extract<keyof ModuleGetters<ModuleName>, string>
-type ModuleGetter<ModuleName extends ModuleNames, GetterName extends ModuleGetterNames<ModuleName>> = ModuleGetters<ModuleName>[GetterName]
+type ModuleGetter<ModuleName extends ModuleNames, GetterName extends ModuleGetterNames<ModuleName>> = Extract<ModuleGetters<ModuleName>[GetterName], (...args: any) => any>
 type ModuleGettersTree<ModuleName extends ModuleNames> = { [K in ModuleGetterNames<ModuleName>]: ComputedRef<ReturnType<ModuleGetter<ModuleName, K>>> }
 
+type ModuleState<ModuleName extends ModuleNames> = Modules[ModuleName]['state'] extends () => any ? ReturnType<Modules[ModuleName]['state']> : Modules[ModuleName]['state']
+type ModuleStateTree<ModuleName extends ModuleNames> = { [K in keyof ModuleState<ModuleName>]: Ref<ModuleState<ModuleName>[K]> }
+
 const options = {
-    registeredStore: null as null | Store
+    registeredStore: null as null | BaseStore
 }
 
-export function registerStore(store: Store) {
+export function registerStore<CurrentStore extends BaseStore>(store: CurrentStore) {
     Object.defineProperty(options, 'registeredStore', { value: store })
 
     return store
 }
 
+export function useModuleStore<ModuleName extends ModuleNames>(name: ModuleName): ModuleActionsTree<ModuleName> & ModuleGettersTree<ModuleName> & ModuleStateTree<ModuleName>
 export function useModuleStore<ModuleName extends ModuleNames>(name: ModuleName) {
     const store = options.registeredStore
+
     if (!store) {
         throw new Error(`No store was registered.`)
     }
@@ -56,9 +59,9 @@ export function useModuleStore<ModuleName extends ModuleNames>(name: ModuleName)
  * 
  * @param name 
  */
-function storeModuleState<ModuleName extends ModuleNames>(store: Store, name: ModuleName) {
+function storeModuleState<ModuleName extends ModuleNames>(store: BaseStore, name: ModuleName) {
     const rootState = store.state as RootState
-    return toRefs(rootState[name] as ModuleState<ModuleName>) as { [K in ModuleState<ModuleName>]: Ref<ModuleState<ModuleName>[K]> }
+    return toRefs(rootState[name])
 }
 
 
@@ -67,12 +70,12 @@ function storeModuleState<ModuleName extends ModuleNames>(store: Store, name: Mo
  * 
  * @param name 
  */
-function storeModuleGetters<ModuleName extends ModuleNames>(store: Store, name: ModuleName) {
+function storeModuleGetters<ModuleName extends ModuleNames>(store: BaseStore, name: ModuleName) {
     const { getters, namespaced } = getRawModule(store, name)
     const tree = {} as ModuleGettersTree<ModuleName>
 
     for (const key in getters) {
-        const treeKey = key as ModuleActionNames<ModuleName>
+        const treeKey = key as ModuleGetterNames<ModuleName>
         const getterName = namespaced ? `${name}/${key}` : key
         tree[treeKey] = computed(() => store.getters[getterName])
     }
@@ -86,7 +89,7 @@ function storeModuleGetters<ModuleName extends ModuleNames>(store: Store, name: 
  * 
  * @param name 
  */
-function storeModuleActions<ModuleName extends ModuleNames>(store: Store, name: ModuleName) {
+function storeModuleActions<ModuleName extends ModuleNames>(store: BaseStore, name: ModuleName) {
     const { actions, namespaced } = getRawModule(store, name)
     const tree = {} as ModuleActionsTree<ModuleName>
 
@@ -102,7 +105,7 @@ function storeModuleActions<ModuleName extends ModuleNames>(store: Store, name: 
     return tree
 }
 
-function getRawModule<ModuleName extends ModuleNames>(store: Store, name: ModuleName) {
+function getRawModule<ModuleName extends ModuleNames>(store: BaseStore, name: ModuleName) {
     const root = (store as any)._modules.root
 
     return root._rawModule.modules[name] as Module<any, any>
